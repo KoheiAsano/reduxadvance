79a903ea3f939d8439eb66969780ef8985cf7a4c
KoheiAsano <komerumeru@i.softbank.jp>
2018-10-14 10:45:12 +0900

    fix typo

--- src/containers/AsyncApp.js
+++ src/containers/AsyncApp.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import {
-  selectSubreddit
+  selectSubreddit,
   fetchPostsIfNeeded,
   invalidateSubreddit
 } from '../actions'
@@ -86,7 +86,7 @@ function mapStateToProps(state) {
     isFetching,
     lastUpdated,
     items: posts
-  } = postsBySubreddit{selectedSubreddit} || {
+  } = postsBySubreddit[selectedSubreddit] || {
     isFetching: true,
     items: []
   }
